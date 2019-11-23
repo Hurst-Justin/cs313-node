e433b5c91c7bd9f7c7bb03ab59e6ab869b700d1a
Justin Hurst <jhurst@gmail.com>
2019-11-23 14:58:03 -0600

    Update Files

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ function getMovie(req,res){
         } else {
             // var result = {id: id, title: title, releasedate: releasedate};
             // res.render('results.ejs', result);
-            res.json(result[0]);
+            res.render(json(result[0]));
         }
     })
 
