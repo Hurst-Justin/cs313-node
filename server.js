da17d56e01cc3e30035ee9398f828aea5dc6aa12
Justin Hurst <jhurst@gmail.com>
2019-11-30 12:11:23 -0600

    Update Files

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const pool = new Pool({connectionString: connectionString});
 
 
 app.set("port", (process.env.PORT || 5000))
-app.get('/', function(req, res){res.sendFile('movieSearch-xhr.html', { root: __dirname + '/public'});})
+app.get('/', function(req, res){res.sendFile('movieSearch.html', { root: __dirname + '/public'});})
 app.use(express.static(__dirname + '/public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
