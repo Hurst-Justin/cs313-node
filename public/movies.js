290794ceb42aa7e4dff94d4c46a9070e753f356a
Justin Hurst <jhurst@gmail.com>
2019-12-07 22:03:31 -0600

    Update Files

--- public/movies.js
+++ public/movies.js
@@ -58,9 +58,9 @@ function search() {
       }
     }
   }
-  const { Pool } = require("pg");
-  const connectionString = process.env.DATABASE_URL || "postgres://familyhistoryuser:elijah@localhost:5432/familyhistory";
-  const pool = new Pool({connectionString: connectionString});
+  // const { Pool } = require("pg");
+  // const connectionString = process.env.DATABASE_URL || "postgres://familyhistoryuser:elijah@localhost:5432/familyhistory";
+  // const pool = new Pool({connectionString: connectionString});
 
   function addMovie(movie_id){
     console.log("adding movies with id:  ", movie_id);
