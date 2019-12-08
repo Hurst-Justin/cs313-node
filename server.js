778be1f56df2f61b2e701730384085121d68754a
Justin Hurst <jhurst@gmail.com>
2019-12-07 21:15:03 -0600

    Update Files

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ function getMovie(req,res){
 function getMovieFromDb(id, callback){
     console.log("getMovieFromDb called with id:  ", id);
 
-    var sql = "SELECT id, title, releasedate, mpaa, genre, director, runtime, studio, summary FROM movies WHERE id = $1::int";
+    var sql = "SELECT id, title, releasedate, mpaa, genre, director, runtime, studio, summary, movie_id FROM movies WHERE id = $1::int";
     var params = [id];
 
     pool.query(sql, params, function(err, result) {
