c65260d6aef365e07b36825a91bd9fab006fffdc
Justin Hurst <jhurst@gmail.com>
2019-12-07 21:10:09 -0600

    Update Files

--- server.js
+++ server.js
@@ -12,47 +12,47 @@ app.get('/', function(req, res){res.sendFile('movieSearch.html', { root: __dirna
 app.use(express.static(__dirname + '/public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
-// app.get("/getMovie", getMovie);
+app.get("/getMovie", getMovie);
 // app.get("/search", search);
 app.listen(app.get("port"), function() {
     console.log("Now listening for connections on port:  ", app.get("port"));
 });
 
-// function getMovie(req,res){
-//     console.log("Getting movies information.");
+function getMovie(req,res){
+    console.log("Getting movies information.");
 
-//     var id = req.query.id;
-//     console.log("Retrieving movies with id:  ", id);
+    var id = req.query.id;
+    console.log("Retrieving movies with id:  ", id);
 
-//     getMovieFromDb(id, function(error, result) {
-//         if(error || result == null || result.length != 1){
-//             res.status(500).json({success:false, data:error});
-//         } else {
-//             // var result = {id: id, title: title, releasedate: releasedate};
-//             // res.render('results.ejs', result);
-//             res.json(result[0]);
-//         }
-//     })
+    getMovieFromDb(id, function(error, result) {
+        if(error || result == null || result.length != 1){
+            res.status(500).json({success:false, data:error});
+        } else {
+            // var result = {id: id, title: title, releasedate: releasedate};
+            // res.render('results.ejs', result);
+            res.json(result[0]);
+        }
+    })
 
-// }
+}
 
-// function getMovieFromDb(id, callback){
-//     console.log("getMovieFromDb called with id:  ", id);
+function getMovieFromDb(id, callback){
+    console.log("getMovieFromDb called with id:  ", id);
 
-//     var sql = "SELECT id, title, releasedate, mpaa, genre, director, runtime, studio, summary FROM movies WHERE id = $1::int";
-//     var params = [id];
+    var sql = "SELECT id, title, releasedate, mpaa, genre, director, runtime, studio, summary FROM movies WHERE id = $1::int";
+    var params = [id];
 
-//     pool.query(sql, params, function(err, result) {
-//         if (err) {
-//             console.log("An errror with the DB occurred");
-//             console.log(err);
-//             callback(err, null);
-//         }
+    pool.query(sql, params, function(err, result) {
+        if (err) {
+            console.log("An errror with the DB occurred");
+            console.log(err);
+            callback(err, null);
+        }
 
-//         console.log("Found DB result:  ", JSON.stringify(result.rows));
+        console.log("Found DB result:  ", JSON.stringify(result.rows));
 
-//         callback(null, result.rows);
+        callback(null, result.rows);
 
-//     })
-// }
+    })
+}
 
