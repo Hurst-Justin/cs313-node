027b61bb74157be3216f22219a23f5a93a4a41ae
Justin Hurst <jhurst@gmail.com>
2019-12-07 23:43:10 -0600

    Update Files

--- public/movies.js
+++ public/movies.js
@@ -65,9 +65,8 @@ function search() {
     content = '<h1>Added Movie:</h1>'
     resultList.innerHTML += content + '<a href="https://www.themoviedb.org/movie/'+ movie_id + '" target="_blank">' + title + ' (' + year + ')</a>'
 
-    // var sql = "INSERT INTO (movie_id) VALUES ("+ movie_id +")";
-    // console.log("SQL:  " + sql);
-    server.addMovieToDB(movie_id);
-    
+    var sql = "INSERT INTO (movie_id) VALUES ("+ movie_id +")";
+    console.log("SQL:  " + sql);
+    server.pool.query(sql);   
 
  }
